d346c609792375c41d7aa81f5698037328bc296c
Dayong Wang <wangdayong228@163.com>
2020-10-09 23:14:32 +0800

    disc note done

--- link.js
+++ link.js
@@ -2,24 +2,6 @@ const fs = require("fs")
 const { join } = require("path")
 
 const root = "./pic"
-// const dir = fs.readdirSync(root)
-// console.log(dir)
-// dir.forEach(d => {
-//     // if (d != ".DS_Store") {
-//         // let files = fs.readdirSync(`./pic/${d}`)
-//         // console.log(files)
-//         // files.map(f=>`![${f}](./pic/${d}/${f})`).forEach(f=>console.log(f))
-//     // }
-//     let subDir = `${root}/${d}`
-//     fs.stat(subDir, (err,stats)=>{
-//         console.log(subDir, stats.isDirectory())
-//         if(stats.isDirectory()){
-//             printFiles(subDir)
-//         }else{
-
-//         }
-//     })
-// })
 
 printFiles(`${root}/1`)
 printFiles(`${root}/2/1green`)
@@ -32,12 +14,17 @@ printFiles(`${root}/4`)
 function printFiles(dir) {
     path = dir
     let files = fs.readdirSync(path)
-    files.map(f => `![${f}](${path}/${f})`).forEach(f => {
-        if (f.indexOf(".DS_Store") < 0) {
-            console.log("[" + f.replace("!", "").substr(3))
-            if (f.indexOf(".mp4") < 0) {
-                console.log(f)
-            }
+    files.map(f => {
+        return {
+            name: f.substr(2).replace(".jpeg", "").replace(".mp4", "").replace(".png", ""),
+            link: f.indexOf(".mp4") >= 0 ? `[${f}](${path}/${f})` : `![${f}](${path}/${f})`
+        }
+    }).forEach(item => {
+        if (item.name.indexOf("_Store") < 0) {
+            console.log(item.name)
+            // if (item.link.indexOf(".mp4") < 0) {
+            console.log(item.link)
+            // }
             console.log()
         }
     })
